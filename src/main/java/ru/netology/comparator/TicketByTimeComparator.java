3050a1448dffbcf2430ba30b508607acf0426043
Boolgakova <boolgakova@gmail.com>
2022-07-08 16:33:10 +0300

    Comparator added

---
+++ src/main/java/ru/netology/comparator/TicketByTimeComparator.java
@@ -0,0 +1,17 @@
+package ru.netology.comparator;
+import ru.netology.domain.Ticket;
+
+import java.util.Comparator;
+
+public class TicketByTimeComparator implements Comparator<Ticket> {
+    @Override
+    public int compare(Ticket o1, Ticket o2) {
+        if (o1.getTimeInMinute() < o2.getTimeInMinute()) {
+            return -1;
+        } else if (o1.getTimeInMinute() > o2.getTimeInMinute()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
